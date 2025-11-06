c9f2f4c79ffdad2b6de9b03c736289b13152dd21
orion <490757236@qq.com>
2025-11-06 17:47:16 +0800

    [CG-30][fix] remove useless =

--- worklenz-frontend/src/api/sprint/sprint.api.service.ts
+++ worklenz-frontend/src/api/sprint/sprint.api.service.ts
@@ -3,17 +3,31 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:8080';
 
 export const sprintService = {
-    // 根据project_id查询sprints
-    getSprintsByProjectId: async (projectId: string) => {
-        const response = await axios.get(
-            `${BASE_URL}/sprints/getByProjectId/${projectId}`
+    addSprints: async (projectId: string) => {
+        const response = await axios.post(
+            `${BASE_URL}/sprints/add`,
+            {
+                projectId: projectId,
+                name: "Sprint New without id",
+                startDate: "2025-11-06",
+                endDate: "2025-11-20",
+                goal: "试着去掉id",
+                description: "新增 Sprint，用于测试创建接口"
+            }
         );
         return response.data;
     },
-    // 根据project_id查询sprints
-    getSprints: async (page: number=1) => {
+    // '4cf4375f-d0fe-4343-9e9e-a8db9b7a0325'
+    setSprintToTask: async (taskId: string ) => {
+        const response = await axios.put(
+            `${BASE_URL}/tasks/${taskId}/sprint?sprintId=1`
+        );
+        return response.data;
+    },
+
+    getSprints: async (page: number = 1) => {
         const response = await axios.get(
-            `${BASE_URL}/sprints/page?=page=${page}`
+            `${BASE_URL}/sprints/page?page=${page}`
         );
         return response.data;
     },
