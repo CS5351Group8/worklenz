56c9a046cd1e7d02f9c7dba5c1896d32cec4187b
orion <490757236@qq.com>
2025-11-27 17:17:18 +0800

    [CG-39][fix] use priorityName instead of priority

--- worklenz-frontend/src/types/project/projectTasksViewModel.types.ts
+++ worklenz-frontend/src/types/project/projectTasksViewModel.types.ts
@@ -94,6 +94,7 @@ export interface IProjectTask {
     project_use_manual_progress?: boolean;
     project_use_time_progress?: boolean;
     project_use_weighted_progress?: boolean;
+    priorityName?: string;
 }
 
 export interface IProjectTasksViewModel {
